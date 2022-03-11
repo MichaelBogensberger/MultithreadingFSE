9eba6dc6817d7c2dcbd42278e6c390f7c68bb4f5
Michi <bogensbergermichael@gmail.com>
2022-03-11 19:01:55 +0100

    changes

--- src/_20220308_SYNC/_05_hash_finder_sync.java
+++ src/_20220308_SYNC/_05_hash_finder_sync.java
@@ -3,6 +3,7 @@ package _20220308_SYNC;
 import java.io.UnsupportedEncodingException;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Set;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -14,22 +15,33 @@ public class _05_hash_finder_sync {
     public static void main(String[] args) {
 
         String data = "Transaktion472";
-        String difficulty = "000";
+        String difficulty = "00000";
 
+        ArrayList<Thread> threads = new ArrayList<>();
         Lock lock = new ReentrantLock();
-        Thread t1 = new Thread(new Job5(data,difficulty,lock), "1");
-        Thread t2 = new Thread(new Job5(data,difficulty,lock), "2");
-        t1.start();
-        t2.start();
-        try {
-            t1.join();
-            t2.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
+
+        int thread_count = 5;
+
+        for(int i = 0; i < thread_count; i++){
+            threads.add(new Thread(new Job5(data,difficulty,lock, thread_count, i), "Thread-"+String.valueOf(i)));
+        }
+
+        for (Thread thread : threads) {
+            thread.start();
+
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
 
     }
 }
+
 
 
 class Job5 implements Runnable {
@@ -37,18 +49,21 @@ class Job5 implements Runnable {
     String data;
     String difficulty;
     Lock lock;
-    DataStorage dataStorage = new DataStorage();
+
 
     boolean isFound = false;
-    long nonce = 0;
-    //long nonce = dataStorage.getNonce();
+    long nonce;
+    int threads;
 
 
 
-    public Job5(String data,String difficulty, Lock lock){
+
+    public Job5(String data,String difficulty, Lock lock, int threads, int nonce){
         this.data= data;
         this.difficulty = difficulty;
         this.lock = lock;
+        this.nonce = nonce;
+        this.threads = threads;
     }
 
     @Override
@@ -56,27 +71,31 @@ class Job5 implements Runnable {
 
 
 
-        while(!isFound && dataStorage.getNonce() < Long.MAX_VALUE) {
-            if (dataStorage.isWorkingOn() == true) {
-                dataStorage.setNonce(dataStorage.getNonce()+1);
-            }
+        while(!isFound && nonce < Long.MAX_VALUE) {
 
 
-            dataStorage.setWorkingOn(true);
+
 
             //String strToHash = data + nonce;
-            String strToHash = data + dataStorage.getNonce();
+            String strToHash = data + nonce;
             String hash = calculateHash(strToHash);
             //System.out.println(hash + " | " + Thread.currentThread().getName());
 
+            // try {
+            // 	Thread.sleep(1000);
+            // } catch (InterruptedException e) {
+            // 	e.printStackTrace();
+            // }
             if(hash.startsWith(difficulty)){
                 //isFound = true;
-                System.out.println("found" + " | " + Thread.currentThread().getName() + " | " + hash + " | " +dataStorage.getNonce() + " | " +dataStorage.isWorkingOn() );
+                System.out.println("found" + " | " + Thread.currentThread().getName() + " | " + hash + " | " +nonce );
+                // System.out.println("found" + " | " + Thread.currentThread().getName() + " | " + hash + " | " +nonce );
             }
 
-            dataStorage.setNonce(dataStorage.getNonce()+1);
-            //nonce ++;
-            dataStorage.setWorkingOn(false);
+
+            nonce = nonce + threads;
+
+
         }
 
 
@@ -84,27 +103,29 @@ class Job5 implements Runnable {
 
 
 
-class DataStorage {
-    private long nonce = 0;
-    private boolean workingOn = false;
+    public static String calculateHash(String strToHash) {
 
-    public long getNonce() {
-        return nonce;
+        byte[] bytesOfMessage;
+        try {
+            bytesOfMessage = strToHash.getBytes("UTF-8");
+            MessageDigest md;
+            md = MessageDigest.getInstance("SHA-256");
+
+            byte[] thedigest = md.digest(bytesOfMessage);
+            StringBuffer hexString = new StringBuffer();
+            for (int i = 0; i < thedigest.length; i++) {
+                String hex = Integer.toHexString(0xff & thedigest[i]);
+                if(hex.length() == 1)
+                    hexString.append('0');
+                hexString.append(hex);
+            }
+            return hexString.toString();
+        }
+        catch (UnsupportedEncodingException | NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        }
+
     }
-    public void setNonce(long nonce) {
-        this.nonce = nonce;
-    }
-
-    public boolean isWorkingOn() {
-        return workingOn;
-    }
-
-    public void setWorkingOn(boolean workingOn) {
-        this.workingOn = workingOn;
-    }
-
-}
-
 
 
 
